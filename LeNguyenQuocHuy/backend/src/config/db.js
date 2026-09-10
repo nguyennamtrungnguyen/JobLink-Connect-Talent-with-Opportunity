045fbcd008006b4bb21c8c5d6505e98f740ea56d
Huy <quochuybt2005@gmail.com>
2026-09-11 01:15:22 +0700

    feat: add backend

---
+++ LeNguyenQuocHuy/backend/src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import ENV from "./env.js";
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(ENV.MONGO_URI);
+    console.log("Connected to DB SUCCESSFULLY");
+  } catch (error) {
+    console.log("Error connecting to Mongodb");
+    process.exit(1);
+  }
+};
